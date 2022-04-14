d6097c09c60dfda2d39fd99d1a15bd36bd502fa4
Andres <andtruj23@gmail.com>
2022-04-13 23:14:23 -0500

    Changed CSS and JS to add Test profiles with margins and alignment of header one.

--- src/client/App.js
+++ src/client/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
 
-
+import React, {useState} from 'react'
+import '../../assets/css/style.css'
 
 const App = () => {
-	return(
-	<div>Hello World!</div>
+	const [post, setPosts] = useState(initialPosts);
+		
+	return (
+		<div className ="container">
+			<div className="feed">
+			{
+				initialPosts.map((post,i) =>
+				<div key={post.id} className="post">
+				  <div className="header">
+				    <img src={post.user.avatar} />
+				    <h2>{post.user.username}</h2>
+				  </div>
+				  <p className="content">
+					{post.text}
+				  </p>
+				</div>
+			)}
+			</div>
+		</div>
 	)
 }
 
@@ -14,9 +31,8 @@ const initialPosts = [
 		id:2,
 		text:'Lorem Ipsum',
 		user:{
-			avatar:'/uploads/avatar1.png',
+			avatar:'/uploads/avatar11.jpeg',
 			username: 'Test User',
-			
 			}
 	},
 	
@@ -24,9 +40,8 @@ const initialPosts = [
 		id:1,
 		text:'Lorem ipsum',
 		user:{
-			avatar: '/uploads/avatar2.png',
+			avatar: '/uploads/avatar22.jpeg',
 			username: 'Test User 2',
-			
 			}
 	}
 	
